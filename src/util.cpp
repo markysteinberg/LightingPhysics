2dd5b52cb7af7e810a946968e930f3ded014da54
markysteinberg <mark62959@gmail.com>
2026-04-26 16:25:10 -0400

    fixed

--- src/util.cpp
+++ src/util.cpp
@@ -1,14 +1,12 @@
 #include "util.h" 
 
-namespace Util { 
-    bool is_key_pressed_once(GLFWwindow* window, int key) {
-        static bool last[GLFW_KEY_LAST + 1] = { false };
+bool is_key_pressed_once(GLFWwindow* window, int key) {
+    static bool last[GLFW_KEY_LAST + 1] = { false };
 
-        bool now = glfwGetKey(window, key) == GLFW_PRESS; 
+    bool now = glfwGetKey(window, key) == GLFW_PRESS; 
 
-        bool pressed = now && !last[key];
-        last[key] = now;
+    bool pressed = now && !last[key];
+    last[key] = now;
 
-        return pressed; 
-    }
+    return pressed; 
 }
